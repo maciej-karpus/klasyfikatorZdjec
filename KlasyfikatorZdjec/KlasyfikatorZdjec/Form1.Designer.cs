64a302d3b1f8dcdd756a58569f0e37ad8d0ff049
mankers <mankersss@gmail.com>
2014-03-30 15:16:12 +0200

    Dodana metoda loadImages sluzaca do wyswietlania miniatur

--- KlasyfikatorZdjec/KlasyfikatorZdjec/Form1.Designer.cs
+++ KlasyfikatorZdjec/KlasyfikatorZdjec/Form1.Designer.cs
@@ -29,11 +29,40 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView
+            // 
+            this.listView.Location = new System.Drawing.Point(336, 13);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(389, 437);
+            this.listView.TabIndex = 3;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 462);
+            this.Controls.Add(this.listView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ListView listView;
     }
 }
 
--- KlasyfikatorZdjec/KlasyfikatorZdjec/Form1.cs
+++ KlasyfikatorZdjec/KlasyfikatorZdjec/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,91 @@ namespace KlasyfikatorZdjec
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loadImages(string directory)
+        {
+            imageList.ImageSize = new Size(100, 100);
+            imageList.ColorDepth = ColorDepth.Depth32Bit;
+
+            string[] extensions = { "jpg", "jpeg", "png", "bmp" };
+            string[] files = Directory.GetFiles(directory, "*.*")
+                .Where(f => extensions.Contains(f.Split('.').Last().ToLower())).ToArray();
+
+            int i = 0;
+            foreach (string file in files)
+            {
+                Image img = Image.FromFile(file);
+                imageList.Images.Add(getThumbnailImage(imageList.ImageSize.Width, img));
+                listView.Items.Add(file);
+                listView.Items[i].ImageIndex = i;
+                i++;
+            }
+
+            listView.View = View.LargeIcon;
+            listView.LargeImageList = imageList;
+        }
+
+        private Image getThumbnailImage(int width, Image img)
+        {
+            Image thumb = new Bitmap(width, width);
+            Image tmp = null;
+
+            if (img.Width < width && img.Height < width)
+            {
+                using (Graphics g = Graphics.FromImage(thumb))
+                {
+                    int xoffset = (int)((width - img.Width) / 2);
+                    int yoffset = (int)((width - img.Height) / 2);
+                    g.DrawImage(img, xoffset, yoffset, img.Width, img.Height);
+                }
+            }
+            else
+            {
+                Image.GetThumbnailImageAbort myCallback = new Image.GetThumbnailImageAbort(ThumbnailCallback);
+
+                if (img.Width == img.Height)
+                {
+                    thumb = img.GetThumbnailImage(width, width, myCallback, IntPtr.Zero);
+                }
+                else
+                {
+                    int k = 0;
+                    int xoffset = 0;
+                    int yoffset = 0;
+
+                    if (img.Width < img.Height)
+                    {
+                        k = (int)(width * img.Width / img.Height);
+                        tmp = img.GetThumbnailImage(k, width, myCallback, IntPtr.Zero);
+                        xoffset = (int)((width - k) / 2);
+                    }
+
+                    if (img.Width > img.Height)
+                    {
+                        k = (int)(width * img.Height / img.Width);
+                        tmp = img.GetThumbnailImage(width, k, myCallback, IntPtr.Zero);
+                        yoffset = (int)((width - k) / 2);
+                    }
+
+                    using (Graphics g = Graphics.FromImage(thumb))
+                    {
+                        g.DrawImage(tmp, xoffset, yoffset, tmp.Width, tmp.Height);
+                    }
+                }
+            }
+
+            using (Graphics g = Graphics.FromImage(thumb))
+            {
+                g.DrawRectangle(Pens.Green, 0, 0, thumb.Width - 1, thumb.Height - 1);
+            }
+
+            return thumb;
+        }
+
+        public bool ThumbnailCallback()
+        {
+            return true;
         }
     }
 }
