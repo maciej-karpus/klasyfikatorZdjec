46627d8373c1942908e4be0cdc0a745bac53af04
mankers <mankersss@gmail.com>
2014-04-04 14:46:40 +0200

    Stworzenie klasy Filter i zmiana sposobu przekazywania parametrow do metody loadImages

---
+++ KlasyfikatorZdjec/KlasyfikatorZdjec/FIlter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasyfikatorZdjec
+{
+    public static class Filter
+    {
+        public static string[] filter(bool[] filters, object[] values, List<classifiedImage> images)
+        {
+            for (int i = 0; i < filters.Length; i++)
+            {
+                if (filters[i])
+                {
+                    foreach (classifiedImage ci in images)
+                    {
+                        if(ci.getProp(i) != values[i])
+                        {
+                            images.Remove(ci);
+                        }
+                    }
+                }
+            }
+            string[] files = new string[images.Count];
+            int j = 0;
+            foreach (classifiedImage ci in images)
+            {
+                files[j] = ci.path;
+            }
+            return files;
+        }
+    }
+}
